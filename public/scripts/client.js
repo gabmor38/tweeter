64bd2b19fd0d2d40394838a9e66e542d6131cff5
gabmor38 <Gabriela.morenor@gmail.co>
2021-09-15 22:18:46 -0400

    added GET and loadTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,30 +10,30 @@
 $(document).ready(()=> {
 
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 
 const renderTweets = function(data) {
@@ -95,5 +95,19 @@ $("button:submit").click(function(event) {
     });
     
   });
+
+// FETCH THE TWEETS//
+
+  const loadTweets = function() {
+
+    $.ajax('/tweets', {method: 'GET', datatype: "json"})
+    .then(function(res) {
+      renderTweets(res);
+
+    });
+
+  
+
+  }
 
 });
